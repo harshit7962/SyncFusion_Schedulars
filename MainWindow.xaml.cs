d52d4c7ef16da881347d35e0fd5ffad2bf8b903a
hmishra <hmishra@microsoft.com>
2025-02-12 17:36:24 +0530

    Adding basic SfScheduler

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Syncfusion.SfSkinManager;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SyncFusion_Schedulars;
 
@@ -19,5 +11,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SfSkinManager.SetTheme(this, new Theme("Windows11Dark"));
     }
 }
